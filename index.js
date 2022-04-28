e5ee8cb87c249847f1e2538101b2eefe7e531f61
kiettnguyen <tknguyen729@gmail.com>
2022-04-28 18:31:56 -0400

    inital commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var userPoolId = "us-west-2_U0pr265Bb";
+var clientId ="80q3im5c1lk02g5b1vb38028d";
+
+var poolData = { UserPoolId : userPoolId,
+  ClientId : clientId
+};
+
+var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+function login(){
+  console.log("Success go to JS");
+  var username = document.getElementById("username").value;
+  var authenticationData = {
+    Username: username,
+    Password: document.getElementById("password").value
+  };
+
+  var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+
+  var userData = {
+    Username : username,
+    Pool : userPool
+  };
+  var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+  console.log(cognitoUser);
+  cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: function (result) {
+      var accessToken = result.getAccessToken().getJwtToken();
+      console.log('Authentication successful', accessToken);
+      window.location = './index.html';
+    },
+
+    onFailure: function(err) {
+      console.log('failed to authenticate');
+      console.log(JSON.stringify(err));
+      alert('Failed to Log in.\nPlease check your credentials.');
+    },
+  });
+}
+
