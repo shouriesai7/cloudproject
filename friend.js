a049876e5af512b30efd67afc29eb64fef3cc7b1
shouriesai <shouriesai7@gmail.com>
2022-05-15 13:53:46 -0400

    Adding Friend Functionality

--- friend.js
+++ friend.js
@@ -8,9 +8,17 @@ var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
 window.populate_with_friend=function()
 {
+
   var friendname = window.sessionStorage.getItem('friend_name');
   var friendemail = window.sessionStorage.getItem('friend_email');
-
+  console.log(userPool.getCurrentUser()['username'])
+  console.log(friendname);
+  if(friendname.trim()==userPool.getCurrentUser()['username'].trim())
+  {
+    console.log(userPool.getCurrentUser()['username'])
+    console.log(friendname);
+    window.location.href = "profile.html";
+  }
 
   var namefriend=document.getElementById("name_div_f");
   var emailfriend=document.getElementById("email_div_f");
@@ -18,8 +26,157 @@ window.populate_with_friend=function()
   namefriend.innerHTML=friendname;
   emailfriend.innerHTML=friendemail;
 
+
+
+
+
+
+  const AWS = require("aws-sdk");
+  AWS.config = new AWS.Config();
+  AWS.config.accessKeyId = "AKIA5JDBPHZNGUXOZVPP";
+  AWS.config.secretAccessKey = "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";
+  AWS.config.update({region: "us-east-1"});
+
+  const lambda = new AWS.Lambda();
+
+  var data ={ 'user1': userPool.getCurrentUser()['username'],
+              'user2':document.getElementById('name_div_f').textContent,
+              'status':'check_status'
+                };
+
+  var datapayload = JSON.stringify(data);
+
+  const params = {
+    FunctionName: "friends",
+    InvocationType: "RequestResponse",
+    Payload: datapayload,
+      LogType: 'None'
+  };
+  var returndata;
+  console.log("yes");
+  lambda.invoke(params, function(error, data) {
+      if (error) {
+          console.log(error);
+      }
+      else {
+          returndata = JSON.parse(data.Payload);
+          var status=returndata['final_status'];
+          console.log(status);
+          var friend_button=document.getElementById('add_remove');
+
+          if(status=="no_connection")
+          {
+              friend_button.innerHTML="ADD";
+          }
+          else if(status=="friends")
+          {
+            friend_button.innerHTML="Remove Friend";
+          }
+          else if(status=="user1_request")
+          {
+            friend_button.innerHTML="Request Sent";
+          }
+          else {
+            friend_button.innerHTML="ADD BACK";
+            var friend_button2=document.createElement("button");
+            friend_button2.innerHTML = "Reject Request";
+            friend_button2.type = "button";
+            friend_button2.className = "btn";
+            friend_button2.setAttribute("id","second_friend_button");
+            friend_button2.className += "btn-primary"
+            friend_button2.className += "btn-lg"
+            friend_div=document.getElementById('friend_button_div')
+            friend_div.appendChild(friend_button2);
+
+
+          }
+
+
+      }
+  });
+
 }
 
+window.add_or_remove_friend=function()
+{
+  var friend_button=document.getElementById('add_remove');
+  if (friend_button.textContent=="ADD")
+  {
+    var status="add_friend";
+
+  }
+  else if(friend_button.textContent=="Remove Friend")
+  {
+    var status="remove_friend";
+
+  }
+  else if(friend_button.textContent=="Request Sent")
+  {
+    return;
+
+  }
+  else {
+    var status="accept_request";
+
+  }
+
+  const AWS = require("aws-sdk");
+  AWS.config = new AWS.Config();
+  AWS.config.accessKeyId = "AKIA5JDBPHZNGUXOZVPP";
+  AWS.config.secretAccessKey = "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";
+  AWS.config.update({region: "us-east-1"});
+
+  const lambda = new AWS.Lambda();
+
+  var data ={ 'user1':userPool.getCurrentUser()['username'],
+              'user2':document.getElementById('name_div_f').textContent,
+              'status':status
+                };
+
+  var datapayload = JSON.stringify(data);
+
+  const params = {
+    FunctionName: "friends",
+    InvocationType: "RequestResponse",
+    Payload: datapayload,
+      LogType: 'None'
+  };
+  var returndata;
+  console.log("yes");
+  lambda.invoke(params, function(error, data) {
+      if (error) {
+          console.log(error);
+      }
+      else {
+          returndata = JSON.parse(data.Payload);
+          var status=returndata['final_status'];
+          var friend_button=document.getElementById('add_remove');
+
+          if(status=="request_accepted")
+          {
+              friend_button.innerHTML="Remove Friend";
+          }
+          else if(status=="request_sent")
+          {
+            friend_button.innerHTML="Request Sent";
+          }
+          else if(status=="request_rejected")
+          {
+            friend_button.innerHTML="ADD";
+          }
+          else {
+            friend_button.innerHTML="ADD";
+
+
+          }
+
+
+      }
+  });
+
+
+
+}
 
 
 //AWS.config.credentials.get(function(err) {
--- profile.js
+++ profile.js
@@ -11,51 +11,303 @@ window.set_info =function()
 {
   var div1=document.getElementById("name_div");
   div1.innerHTML=userPool.getCurrentUser()['username'];
-}
 
-function login(){
-  console.log("Success go to JS");
-  var username = document.getElementById("username").value;
-  var authenticationData = {
-    Username: username,
-    Password: document.getElementById("password").value
+  if(document.getElementById('restaurant_list_div'))
+  {
+    document.getElementById('restaurant_list_div').clear();
+  }
+
+  const AWS = require("aws-sdk");
+  AWS.config = new AWS.Config();
+  AWS.config.accessKeyId = "AKIA5JDBPHZNGUXOZVPP";
+  AWS.config.secretAccessKey = "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";
+  AWS.config.update({region: "us-east-1"});
+
+  const lambda = new AWS.Lambda();
+
+  var data ={ 'user1': userPool.getCurrentUser()['username'],
+              'status':'check_request'
+                };
+
+
+  var datapayload = JSON.stringify(data);
+
+  const params = {
+    FunctionName: "friends",
+    InvocationType: "RequestResponse",
+    Payload: datapayload,
+      LogType: 'None'
   };
+  var returndata;
+  console.log("yes");
+  lambda.invoke(params, function(error, data) {
+      if (error) {
+          console.log(error);
+      }
+      else {
+          returndata = JSON.parse(data.Payload);
+          var status=returndata['final_status'];
+          console.log(returndata)
+          if (returndata['final_status']=="has_pending_requests")
+            {
+              var div = document.createElement('div');
+              div.setAttribute("id", "restaurant_list_div");
+              div.setAttribute('class', 'dynamic_list');
+              document.body.appendChild(div);
+              for (let i=0; i < returndata['requests'].length; i++) {
+                var newlabel3 = document.createElement("label");
+                newlabel3.innerHTML = returndata['requests'][i];
+                newlabel3.setAttribute("id", returndata['requests'][i]);
 
-  var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+                var newlabel = document.createElement("button");
+                newlabel.innerHTML = "Add";
+                newlabel.setAttribute("id", returndata['requests'][i]+"add");
+                var newlabel2 = document.createElement("button");
+                newlabel2.innerHTML = "Cancel";
+                newlabel2.setAttribute("id", returndata['requests'][i]+"cancel");
+                div.appendChild(newlabel3);
+                div.appendChild(newlabel);
+                div.appendChild(newlabel2);
+                var user2=returndata['requests'][i];
+                console.log(returndata['requests'][i]);
+                console.log("aaaaaaaa");
+                newlabel.addEventListener("click", () =>  {
+                  console.log(user2);
+                  console.log("bbbbbbbbb");
 
-  var userData = {
-    Username : username,
-    Pool : userPool
-  };
-  var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+                  const AWS = require("aws-sdk");
+                  AWS.config = new AWS.Config();
+                  AWS.config.accessKeyId = "AKIA5JDBPHZNGUXOZVPP";
+                  AWS.config.secretAccessKey = "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";
+                  AWS.config.update({region: "us-east-1"});
 
-  console.log(cognitoUser);
-  cognitoUser.authenticateUser(authenticationDetails, {
-    onSuccess: function (result) {
-      var accessToken = result.getAccessToken().getJwtToken();
-      console.log('Authentication successful', accessToken);
-      window.location = './mainpage.html';
-    },
+                  const lambda = new AWS.Lambda();
 
-    onFailure: function(err) {
-      console.log('failed to authenticate');
-      console.log(JSON.stringify(err));
-      alert('Failed to Log in.\nPlease check your credentials.');
-    },
+                  var data ={ 'user1':userPool.getCurrentUser()['username'],
+                              'user2':user2,
+                              'status':"accept_request"
+                                };
+
+                  var datapayload = JSON.stringify(data);
+
+                  const params = {
+                    FunctionName: "friends",
+                    InvocationType: "RequestResponse",
+                    Payload: datapayload,
+                      LogType: 'None'
+                  };
+                  var returndata;
+                  console.log("yessssss");
+                  lambda.invoke(params, function(error, data) {
+                      if (error) {
+                          console.log(error);
+                      }
+                      else {
+                          returndata = JSON.parse(data.Payload);
+                          var status=returndata['final_status'];
+
+                          if(status=="request_accepted")
+                          {
+                            newlabel3.remove();
+                            newlabel2.remove();
+                            newlabel1.remove();
+                          }
+
+
+
+                      }
+                  });
+
+
+    });
+
+    newlabel2.addEventListener("click", () =>  {
+
+      const AWS = require("aws-sdk");
+      AWS.config = new AWS.Config();
+      AWS.config.accessKeyId = "AKIA5JDBPHZNGUXOZVPP";
+      AWS.config.secretAccessKey = "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";
+      AWS.config.update({region: "us-east-1"});
+
+      const lambda = new AWS.Lambda();
+
+      var data ={ 'user1':userPool.getCurrentUser()['username'],
+                  'user2':user2,
+                  'status':"reject_request"
+                    };
+
+      var datapayload = JSON.stringify(data);
+
+      const params = {
+        FunctionName: "friends",
+        InvocationType: "RequestResponse",
+        Payload: datapayload,
+          LogType: 'None'
+      };
+      var returndata;
+      console.log("yes");
+      lambda.invoke(params, function(error, data) {
+          if (error) {
+              console.log(error);
+          }
+          else {
+            returndata = JSON.parse(data.Payload);
+            var status=returndata['final_status'];
+
+            if(status=="request_rejected")
+            {
+              newlabel3.remove();
+              newlabel2.remove();
+              newlabel1.remove();
+            }
+
+
+          }
+      });
+
+
+    });
+
+
+
+
+      }
+            }
+
+
+
+      }
   });
+
 }
+
+
 //AWS.config.credentials.get(function(err) {
 //  if (!err) {
   //  var id = AWS.config.credentials.identityId;
   //}
 //});
 
-function opentab(tabname) {
+window.opentab=function(tabname) {
   //for (i = 0; i < 4; i++) {
   //  var name= "tab"+i;
   //  closetab(name);
   //}
-  document.getElementById(tabname).removeAttribute("style");
+
+  const AWS = require("aws-sdk");
+  AWS.config = new AWS.Config();
+  AWS.config.accessKeyId = "AKIA5JDBPHZNGUXOZVPP";
+  AWS.config.secretAccessKey = "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";
+  AWS.config.update({region: "us-east-1"});
+
+  const lambda = new AWS.Lambda();
+
+  var data ={ 'user1': userPool.getCurrentUser()['username'] };
+
+  var datapayload = JSON.stringify(data);
+
+  const params = {
+    FunctionName: "getfriends",
+    InvocationType: "RequestResponse",
+    Payload: datapayload,
+      LogType: 'None'
+  };
+  var returndata;
+  console.log("yes");
+  lambda.invoke(params, function(error, data) {
+      if (error) {
+          console.log(error);
+      }
+      else {
+          returndata = JSON.parse(data.Payload);
+          console.log(returndata);
+          if(document.getElementById('restaurant_list_div'))
+          {
+            document.getElementById('restaurant_list_div').clear();
+          }
+          if(returndata['final_status']=="friends")
+          {
+            var div = document.createElement('div');
+            div.setAttribute("id", "restaurant_list_div");
+            div.setAttribute('class', 'dynamic_list');
+            document.body.appendChild(div);
+            for (let i=0; i < returndata['requests'].length; i++) {
+              var newlabel3 = document.createElement("label");
+              newlabel3.innerHTML = returndata['requests'][i];
+              newlabel3.setAttribute("id", returndata['requests'][i]);
+
+
+              var newlabel2 = document.createElement("button");
+              newlabel2.innerHTML = "Remove Friend";
+              newlabel2.setAttribute("id", returndata['requests'][i]+"remove");
+              div.appendChild(newlabel3);
+              div.appendChild(newlabel2);
+              var user2=returndata['requests'][i];
+              console.log(returndata['requests'][i]);
+              console.log("aaaaaaaa");
+
+  newlabel2.addEventListener("click", () =>  {
+
+    const AWS = require("aws-sdk");
+    AWS.config = new AWS.Config();
+    AWS.config.accessKeyId = "AKIA5JDBPHZNGUXOZVPP";
+    AWS.config.secretAccessKey = "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";
+    AWS.config.update({region: "us-east-1"});
+
+    const lambda = new AWS.Lambda();
+
+    var data ={ 'user1':userPool.getCurrentUser()['username'],
+                'user2':user2,
+                'status':"remove_friend"
+                  };
+
+    var datapayload = JSON.stringify(data);
+
+    const params = {
+      FunctionName: "friends",
+      InvocationType: "RequestResponse",
+      Payload: datapayload,
+        LogType: 'None'
+    };
+    var returndata;
+    console.log("yes");
+    lambda.invoke(params, function(error, data) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+          returndata = JSON.parse(data.Payload);
+          var status=returndata['final_status'];
+
+          if(status=="friend_removed")
+          {
+              newlabel3.remove();
+              newlabel2.remove();
+          }
+
+
+        }
+    });
+
+
+  });
+
+
+
+
+    }
+
+
+          }
+
+
+
+
+
+      }
+  });
+
+  //document.getElementById(tabname).removeAttribute("style");
 }
 
 var myfunc = function( val){
